e5d3fa5be93cd3ae6fa69c6a44db183ebaa03e9b
Skandesh <42321593+Skandesh@users.noreply.github.com>
2024-10-05 01:29:23 +0530

    add secret to env

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+export const RECLAIM_CONFIG = {
+  APP_ID: '0x781Bab493Fe06f40934228a4C383A10B6Cb79631',
+  APP_SECRET: process.env.APP_SECRET,
+  PROVIDER_ID: 'bd95d43f-e06a-4fd7-ac46-3f4c9da284f0',
+};
+
+export const AFFILIATE_ID = 'youraffiliateid';
